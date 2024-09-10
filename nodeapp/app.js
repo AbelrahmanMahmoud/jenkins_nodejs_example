b568836472510749886856d24a8e43b41515ae8d
abdomahmoud <155377254+AbelrahmanMahmoud@users.noreply.github.com>
2024-09-10 11:48:37 +0300

    Update app.js

--- nodeapp/app.js
+++ nodeapp/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 app.get('/', (req, res) => {
-    res.send('Hello World from raya DEPI!')
+    res.send('Hello World from abdoraya DEPI!')
   })
   
   app.listen(port, () => {
